0c2cbf886c371d9dcf5eb1a3852fceb8695cc9a8
luisfe0604 <106082104+luisfe0604@users.noreply.github.com>
2023-11-08 10:56:30 -0300

    search bar

--- store-ZGas/src/components/ActionButtons.jsx
+++ store-ZGas/src/components/ActionButtons.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Input from '@mui/material/Input';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
 
 const actionButton = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = () => {
+        const filtered = data.filter((item) =>
+            Object.values(item).some((value) =>
+                value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+            )
+        );
+        onDataFiltered(filtered);
+    };
+
     return (
-        <div>
-            <button className='action-buttons'>Novo</button>
-            <button className='action-buttons'>Editar</button>
-            <button className='action-buttons'>Excluir</button>
+        <div className='content-actions'>
+            <div className='content-actions'>
+                <button className='action-buttons'>Novo</button>
+                <button className='action-buttons'>Editar</button>
+                <button className='action-buttons'>Excluir</button>
+            </div>
+            <div className='content-actions-input'>
+                <Input
+                    id="search-input"
+                    placeholder="Buscar"
+                    endAdornment={ // Alterado para 'endAdornment'
+                        <InputAdornment position="end">
+                            <SearchIcon onClick={handleSearch} style={{ cursor: 'pointer' }} />
+                        </InputAdornment>
+                    }
+                />
+            </div>
         </div>
+
     );
 };
 
--- store-ZGas/src/components/Header.jsx
+++ store-ZGas/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="header">
-      <h1>ZGÁS</h1>
+      <Link to="/home" className='link-header'>
+        <span>Z</span><small>GÁS</small>
+      </Link>
       <nav className="nav">
         <ul>
-          <li><a href="/home">Página Inicial</a></li>
+          {/* <li><a href="/home">Página Inicial</a></li> */}
           {/* <li><a href="/">Sobre</a></li>
           <li><a href="/">Contato</a></li> */}
         </ul>
